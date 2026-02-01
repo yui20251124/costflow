c2d7d094ce9dd499ba8a0bebed04d0b5d73075b7
yui20251124 <koyanagiyui.913@gmail.com>
2026-02-01 20:52:06 +0900

    Refactor db function to remove hardcoded values
    Removed database connection parameters from db function.

--- db.php
+++ db.php
@@ -1,10 +1,5 @@
 <?php
 function db(): PDO {
-  $host = "127.0.0.1";
-  $db   = "costflow";
-  $user = "root";
-  $pass = ""; // XAMPPデフォルトは空が多い
-  $charset = "utf8mb4";
 
   $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
   return new PDO($dsn, $user, $pass, [
